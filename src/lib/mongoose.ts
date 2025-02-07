1e4319b7088dbcbfd3b79650198be7641a00f0d2
Xasanov Usmonjon <164606236+devusmonjon@users.noreply.github.com>
2025-02-07 14:55:46 +0500

    Update mongoose.ts

--- src/lib/mongoose.ts
+++ src/lib/mongoose.ts
@@ -18,7 +18,7 @@ export const connectToDatabase = async () => {
       dbName: "uc_bot",
       autoCreate: true,
     };
-    await mongoose.connect(process.env.MONGODB_URI!, options);
+    await mongoose.connect("mongodb://localhost:27017/uc_bot", options);
     isConncected = true;
     console.log("MongoDB is connected");
   } catch (err) {
