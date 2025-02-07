34aa1499817d65c59e812527f274eaa3406a346c
Xasanov Usmonjon <164606236+devusmonjon@users.noreply.github.com>
2025-02-07 22:04:10 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    NEXTAUTH_URL: "https://yourdomain.com",
+  },
 };
 
 export default nextConfig;
