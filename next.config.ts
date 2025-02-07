8f02d310cd89d17161daca66ae51cc3a40cca00a
Xasanov Usmonjon <164606236+devusmonjon@users.noreply.github.com>
2025-02-07 22:08:15 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   env: {
-    NEXTAUTH_URL: "https://adminuc.up-it.uc",
+    NEXTAUTH_URL: "https://adminuc.up-it.uz",
   },
 };
 
