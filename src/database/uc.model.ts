fea94198886081ae062965812ce6910eed430242
Xasanov Usmonjon <164606236+devusmonjon@users.noreply.github.com>
2025-02-25 14:41:16 +0500

    Create uc.model.ts

---
+++ src/database/uc.model.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const ucSchema = new mongoose.Schema(
+  {
+    name: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    image: {
+      type: String,
+      required: true,
+      default: null,
+    },
+    price: {
+      type: Number,
+      required: true,
+      unique: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Uc =
+  mongoose.models.Uc || mongoose.model("Uc", ucSchema);
+export default Uc;
